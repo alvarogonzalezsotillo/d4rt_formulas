5108b405e08499faa36fd790d008acfd112aeb22
Álvaro González <alvaro@asus>
2026-03-17 19:30:15 +0100

    dart highlight

--- lib/ai/import_preview_screen.dart
+++ lib/ai/import_preview_screen.dart
@@ -8,7 +8,7 @@ import 'package:d4rt_formulas/services/import_service.dart';
 
 import 'package:flutter_code_editor/flutter_code_editor.dart';
 import 'package:flutter_highlight/themes/monokai-sublime.dart';
-import 'package:highlight/languages/java.dart';
+import 'package:highlight/languages/dart.dart';
 
 /// Screen to preview and import formula elements
 class ImportPreviewScreen extends StatefulWidget {
