707107d7dbbfd874087d7a086b260f33cecfc0b0
Álvaro González <alvarogonzalezsotillo@gmail.com>
2026-08-05 23:10:30 +0200

    Release with origin and branch (gh-pages)

--- release.sh
+++ release.sh
@@ -93,7 +93,7 @@ update_gh_pages(){
     pushd $GHPAGESDIR
     git add .
     git commit -m "Update webapp from release $TAG"
-    git push --force
+    git push --force origin github
     popd
 }
 
